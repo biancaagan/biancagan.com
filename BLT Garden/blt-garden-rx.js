37279b99b19bf50ed2738a45fc66f6d61c46c5cb
biancaagan <90641547+biancaagan@users.noreply.github.com>
2026-04-05 23:05:04 -0400

    update

---
+++ BLT Garden/blt-garden-rx.js
@@ -0,0 +1,107 @@
+// const mqtt = require("mqtt"); // skip in browser
+
+let topic = 'blt';
+let client;
+
+// Divs to show messages:
+let brokerDiv, statusDiv, moisture1Div, moisture2Div;
+// Whether the client should be publishing or not:
+let publishing = true;
+
+function setup() {
+    // Put the divs in variables for ease of use:
+    brokerDiv = document.getElementById('brokerDiv');
+    statusDiv = document.getElementById('statusDiv');
+    moisture1Div = document.getElementById('moisture1Div');
+    moisture2Div = document.getElementById('moisture2Div');
+
+    // Set text of brokerDiv:
+    brokerDiv.innerHTML = 'Trying to connect...';
+
+    // Attempt to connect:
+    client = mqtt.connect("wss://blt-garden-v1:eBikWJ95547eTzBs@blt-garden-v1.cloud.shiftr.io", {
+        clientId: "blt-test-1",
+    });
+
+    // Set listeners:
+    client.on('connect', onConnect);
+    client.on('close', onDisconnect);
+    client.on('message', onMessage);
+    client.on('error', onError);
+
+}
+
+
+// Handler for MQTT connect event:
+function onConnect() {
+    // Update brokerDiv text:
+    brokerDiv.innerHTML = 'Connected to broker.';
+    client.subscribe(topic);
+    // Can subscribe to multiple topics
+}
+
+
+// Handler for MQTT disconnect event:
+function onDisconnect() {
+    // Update brokerDiv text:
+    brokerDiv.innerHTML = 'Disconnected from broker.';
+}
+
+
+// Handler for MQTT error event:
+function onError(error) {
+    // Update brokerDiv text:
+    brokerDiv.innerHTML = error;
+}
+
+
+// Handler for MQTT subscribe event:
+function onSubscribe(response, error) {
+    if (!error) {
+        // Update brokerDiv text:
+        brokerDiv.innerHTML = 'Subscribed to broker.';
+    } else {
+        // Update brokerDiv text with the error:
+        brokerDiv.innerHTML = error;
+    }
+}
+
+
+// Handler for MQTT message received event:
+function onMessage(topic, message) {
+    // Message is a buffer, convert to a string:
+    const msgString = message.toString();
+    const data = JSON.parse(msgString);
+
+    // console.log(topic + ": " + message.toString());
+
+    // try {
+    //     data = JSON.parse(msgString);
+    // } catch (err) {
+    //     console.log("Invalid JSON:", msgString);
+    //     return;
+    // }
+
+    // Individual JSON values:
+    let pumpState = data.state;
+    let moisture1 = data.moisture1;
+    let moisture2 = data.moisture2;
+
+    // Update statusDiv:
+    statusDiv.innerHTML += pumpState;
+    moisture1Div.innerHTML += moisture1;
+    moisture2Div.innerHTML += moisture2;
+}
+
+
+// On page load, call the setup function:
+document.addEventListener('DOMContentLoaded', setup);
+// // Run a loop every 2 seconds:
+// setInterval(loop, 3000);
+
+
+
+
+
+
+
---
+++ BLT Garden/blt-garden-tx.js
@@ -0,0 +1,25 @@
+const mqtt = require("mqtt"); // skip in browser
+
+const client = mqtt.connect("mqtt://blt-garden-v1:LxTKpBMbMyOsQfnz@blt-garden-v1.cloud.shiftr.io", {
+  clientId: "blt-test-2",
+});
+
+client.on("connect", function () {
+  console.log("Connected!");
+
+  client.subscribe("blt");
+
+  setInterval(function () {
+    client.publish("blt", JSON.stringify({
+      state: "Watering...",
+      moisture1: 80,
+      moisture2: 43
+    }), 5000);
+
+  //   client.publish("blt", "{state: Watering..., moisture1: 80}");
+  // }, 5000);
+});
+
+// client.on("message", function (topic, message) {
+//   console.log(topic + ": " + message.toString());
+});
