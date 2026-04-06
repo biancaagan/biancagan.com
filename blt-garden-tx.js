b179c1a3e34bcedf62f0b62f089c4c86112b93fa
biancaagan <90641547+biancaagan@users.noreply.github.com>
2026-04-05 23:32:39 -0400

    update

--- blt-garden-tx.js
+++ blt-garden-tx.js
@@ -1,7 +1,9 @@
 // const mqtt = require("mqtt"); // skip in browser
 
-const client = mqtt.connect("wss://blt-garden-v1:LxTKpBMbMyOsQfnz@blt-garden-v1.cloud.shiftr.io", {
-  clientId: "blt-tx",
+const client = mqtt.connect("wss://blt-garden-v1.cloud.shiftr.io", {
+    clientId: "blt-tx-" + Math.random().toString(16).substr(2, 8),
+    username: "blt-garden-v1",
+    password: "LxTKpBMbMyOsQfnz"
 });
 
 client.on("connect", function () {
@@ -10,11 +12,12 @@ client.on("connect", function () {
   client.subscribe("blt");
 
   setInterval(function () {
-    client.publish("blt", JSON.stringify({
-      state: "Watering...",
-      moisture1: 80,
-      moisture2: 43
-    }), 5000);
+  client.publish("blt", JSON.stringify({
+    state: "Watering...",
+    moisture1: 80,
+    moisture2: 43
+  }));
+}, 5000);
 
     
 
@@ -24,4 +27,3 @@ client.on("connect", function () {
 
 // client.on("message", function (topic, message) {
 //   console.log(topic + ": " + message.toString());
-});
